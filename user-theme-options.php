fe62923c2f2eea5c6688c77b65ef76394a6582d7
Enrico Sorcinelli <enrico.sorcinelli@italiaonline.it>
2022-05-18 09:18:15 +0200

    Increased version

--- user-theme-options.php
+++ user-theme-options.php
@@ -11,7 +11,7 @@
  * Author URI:  https://github.com/enrico-sorcinelli/user-theme-options/graphs/contributors
  * Text Domain: user-theme-options
  * Domain Path: /languages/
- * Version:     1.0.0
+ * Version:     1.1.0
  * License:     GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
@@ -23,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Plugins constants.
-define( 'USER_THEME_OPTIONS_VERSION', '1.0.0' );
+define( 'USER_THEME_OPTIONS_VERSION', '1.1.0' );
 define( 'USER_THEME_OPTIONS_BASEDIR', dirname( __FILE__ ) );
 define( 'USER_THEME_OPTIONS_BASEURL', plugin_dir_url( __FILE__ ) );
 
